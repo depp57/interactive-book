ef30df75f7c801e5b954bba5a98a35fb181b6847
depp57 <deppdu5758@gmail.com>
2021-06-11 23:27:48 +0200

    basic UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  title = 'interactive-book';
+export class AppComponent implements OnInit {
+
+    ngOnInit(): void {
+        const pages = document.getElementsByClassName('page');
+        for (let i = 0; i < pages.length; i++) {
+            const page = pages[i] as HTMLElement;
+            if (i % 2 === 0) {
+                page.style.zIndex = `${(pages.length - i)}`;
+            }
+        }
+
+        document.addEventListener('DOMContentLoaded', function () {
+            for (let i = 0; i < pages.length; i++) {
+                //Or var page = pages[i];
+
+                const page = pages[i] as HTMLElement;
+
+                page.onclick = function () {
+                    if ((i+1) % 2 === 0) {
+                        page.classList.remove('flipped');
+                        page.previousElementSibling?.classList.remove('flipped');
+                    }
+                    else {
+                        page.classList.add('flipped');
+                        page.nextElementSibling?.classList.add('flipped');
+                    }
+                };
+            }
+        });
+    }
 }
